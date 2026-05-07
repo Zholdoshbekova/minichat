1d051b13e7e2434bee519cbebfdd8dff7e17c63a
Zholdoshbekova <zholdoshbekovaaigerim5@gmail.com>
2026-05-08 01:15:08 +0600

    feat: setup basic project structure

---
+++ src/Admin/admin.tsx
@@ -0,0 +1,12 @@
+import './admin.css';
+
+
+interface Admin {}
+
+export const Admin = () => {
+    return (
+        <>
+        </>
+    );
+
+};
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Admin } from './Admin/admin';
+import { Chat }  from './Chat/chat';
+
+
+
+ export const App = () => {
+  return (
+    <div>
+      <Admin />
+      <Chat />
+
+    </div>
+
+
+  );
+};
+
+export default App;
