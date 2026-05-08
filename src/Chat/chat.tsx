3bf732d44cece96f4ece570b13321d74fa367263
Zholdoshbekova <zholdoshbekovaaigerim5@gmail.com>
2026-05-08 13:31:07 +0600

    feat:base project

--- src/Chat/chat.tsx
+++ src/Chat/chat.tsx
@@ -1,16 +1,20 @@
+import { useState } from 'react';
 import './chat.css';
 
 interface Chat {}
 
 export const Chat = () => {
-    return (
-        <>
+    const [ isOpen, setIsOpen ] = useState(false);
 
-         <div className="chat_icon">
-             <img className="icona"  src="/chat.svg" alt="icon" />
-         </div>
+    return ( 
+        <div className='chat_container'>
+            <div className='chat_place'>
+                <textarea className='text' placeholder='world........'/>
+            </div>
 
-        </>
+            <div className='chat_icon' onClick={() => setIsOpen(!isOpen)}>
+                <img className='icona'  src="/chat.svg" alt="icon" />
+            </div>
+        </div>
     );
-
 };
