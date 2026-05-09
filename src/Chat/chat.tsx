77240a480ca0f21f4018465b560ffe3697d3868e
Zholdoshbekova <zholdoshbekovaaigerim5@gmail.com>
2026-05-09 23:21:08 +0600

    SCRUM-24:fix broken SVG structure and update icon paths

--- src/Chat/chat.tsx
+++ src/Chat/chat.tsx
@@ -1,16 +1,83 @@
-import './chat.css';
 
-interface Chat {}
+import { useState, useEffect } from 'react';
+import './chat.css';
+import chatIcon from '../../public/chat.svg';
+
+
+interface Message {
+  id: number;
+  text: string;
+  sender: 'me' | 'other';
+}
 
 export const Chat = () => {
-    return (
-        <>
 
-         <div className="chat_icon">
-             <img className="icona"  src="/chat.svg" alt="icon" />
-         </div>
+  const [isOpen, setIsOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [messages, setMessages] = useState<Message[]>([ 
+    { id: 1, text: "Привет!", sender: 'other' },
+    { id: 2, text: "Как дела?", sender: 'other' },
+  ]);
+ useEffect(() => {
+  const timer = setTimeout(() => {
+    setIsVisible(true); 
+  }, 5000);
 
-        </>
-    );
+    return () => clearTimeout(timer);
+   }, []);
+  const ChatCloseOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSend = () => {
+    if (inputValue.trim() === '') return; 
+
+    
+    const newMessage: Message = {
+      id: Date.now(),
+      text: inputValue,
+      sender: 'me',
+    };
+
+    setMessages([...messages, newMessage]);
+    setInterval('');
+  };
+   if (!isVisible) return null;
+
+  return (
+    <div className={`chat_container ${isOpen ? 'active' : ''}`}>
+      <div className='chat_place'>
+        {/* Список сообщений */}
+        <div className="messages_list">
+          {messages.map((msg) => (
+            <div key={msg.id} className={`message ${msg.sender}`}>
+              {msg.text}
+            </div>
+          ))}
+        </div>
+
+        
+        <div className="input">
+          <textarea 
+            className='text' 
+            placeholder='world.......' 
+            value={inputValue}
+            onChange={(e) => { 
+                setInputValue(e.target.value);
+                e.target.style.height = 'auto';
+                e.target.style.height = e.target.scrollHeight + 'px'; 
+             }}
+             rows={1} 
+          />
+          <button className='send' onClick={handleSend}>send</button>
+        </div>
+      </div>
+
+      <div className='chat_icon' onClick={ChatCloseOpen}>
+        <img className='icona' src={chatIcon} alt="icon" />
+      </div>
+    </div>
+  );
 
 };
