0e25169f6a97cd0331b0bfc491278e7c94aeb88c
Zholdoshbekova <zholdoshbekovaaigerim5@gmail.com>
2026-05-11 15:59:14 +0600

    initial commit

---
+++ src/Chat/chat.tsx
@@ -0,0 +1,16 @@
+import './chat.css';
+
+interface Chat {}
+
+export const Chat = () => {
+    return (
+        <>
+
+         <div className="chat_icon">
+             <img className="icona"  src="/chat.svg" alt="icon" />
+         </div>
+
+        </>
+    );
+
+};
